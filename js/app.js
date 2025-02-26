b2f5cb99af0729c6de0378c5529e501e8198c71c
Samanta portatil <samanta.ortuno@gmail.com>
2025-02-26 21:08:04 +0100

    Eliminamos duplicados con la copia del objeto, añadimos boton de editar y eliminar

--- js/app.js
+++ js/app.js
@@ -53,18 +53,12 @@ class alert {
 
 class citas {
     constructor(){
-        this.cita = [];  
-        this.mostrarCita();      
+        this.citas = [];     
     }
 
     agregarCita(citaAnimal) {        
-      /*   console.log(citaAnimal); */
-     /*    console.log(this.cita); */
-        this.cita = [...this.cita, citaAnimal];
-    /*     console.log('----------------------'); */
-
-        this.mostrarCita();
-                
+        this.citas = [...this.citas, citaAnimal];
+        this.mostrarCita();                
     }
 
     mostrarCita() {
@@ -74,8 +68,8 @@ class citas {
         }
 
         // Generar citas
-        this.cita.forEach(cita => {
-            console.log(cita);
+        this.citas.forEach(cita => {
+            // console.log(cita);
             
             const divCita = document.createElement('div');
             divCita.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10' ,'rounded-xl', 'p-3');
@@ -100,12 +94,35 @@ class citas {
             sintomas.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             sintomas.innerHTML = `<span class="font-bold uppercase">Síntomas: </span> ${cita.sintomas}`;
         
+            // Botones de Editar y Eliminar
+            const btnEditar = document.createElement('button');
+            btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2', 'btn-editar');
+            btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+            const clon = {...cita};
+            btnEditar.onclick = () => editarCita(clon);
+
+
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2', 'btn-eliminar');
+            btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+            // btnEliminar.onclick(() => {
+
+            // })
+
+
+            const contenedorBotones = document.createElement('DIV');
+            contenedorBotones.classList.add('flex','justify-between','mt-10');
+            contenedorBotones.appendChild(btnEditar);
+            contenedorBotones.appendChild(btnEliminar);
+
+
             // Agregar al HTML
             divCita.appendChild(paciente);
             divCita.appendChild(propietario);
             divCita.appendChild(email);
             divCita.appendChild(fecha);
             divCita.appendChild(sintomas);
+            divCita.appendChild(contenedorBotones);
             listaCitas.appendChild(divCita);
         }); 
     }
@@ -162,7 +179,12 @@ function addPatient(e) {
     
     new alert("Paciente añadido correctamente","check")
 
-    citaClass.agregarCita(animal)
+    citaClass.agregarCita({...animal}); // le pasamos una copia {...animal} no el objeto animal
     formulario.reset();  
     resetObj();
+}
+
+function editarCita(cita) {
+    console.log(cita);
+    
 }
